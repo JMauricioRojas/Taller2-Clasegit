4fd2ad5f3f1e8b7a6c632b923e9043b878225a5e
Jaider <Jaiderrojas.jmrr@gmail.com>
2024-04-08 20:55:49 -0500

    cambio en rama pruebas

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
 alert("bienvenidos a taller tecnologico")
 console.log("Esto se esta escribiendo en la consola")
 console.log("Este cambio ira a remoto")
-"
+console.log ("se agrega cambio en rama pruebas")
